eba241dc39a27f9d257dd66314d17ac9085f6f7a
Asus <rishav032005@gmail.com>
2024-10-27 16:43:32 +0530

    Setting Up Frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
@@ -14,8 +15,14 @@ dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 5000;
 
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
 app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
+
 
 
 app.use("/api/v1/auth",authRoutes);
