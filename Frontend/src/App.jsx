254105803b2311376645fffe499e865155548256
Asus <rishav032005@gmail.com>
2024-10-28 21:07:08 +0530

    Adding Navbar to layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,38 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./Components/Layout/Layout"
 
 import HomePage from "./Pages/HomePage"
 import SignUpPage from "./Pages/SignUpPage"
 import LoginPage from "./Pages/LoginPage"
-import { Toaster } from "react-hot-toast"
+import toast, { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import { axiosInstance } from "./lib/axios"
 
 export default function App(){
+
+  const {data:authUser, isLoading}=useQuery({
+    queryKey: ["authUser"],
+    queryFn: async()=>{
+      try {
+        const resp=await axiosInstance.get("/auth/user");
+        return resp.data;
+      } catch (error) {
+        if(error.response && error.response.status===401){
+          return null;
+        }
+        toast.error(error.response.data.message || "Something went wrong")
+      }
+    }
+  })
+
+  if (isLoading) return null;
+
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser?<HomePage />:<Navigate to={"/login"} />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage />: <Navigate to={"/"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage />:<Navigate to="/" />} />
       </Routes>
       <Toaster />
     </Layout>
--- Frontend/src/Components/Layout/Layout.jsx
+++ Frontend/src/Components/Layout/Layout.jsx
@@ -1,7 +1,11 @@
 import React, { Children } from 'react'
 import Navbar from './Navbar'
+import { useQuery } from '@tanstack/react-query'
 
 const Layout = ({children}) => {
+  const {data:authUser, isLoading}=useQuery({queryKey:["authUser"]});
+  console.log("Auth user is in Layout",authUser);
+
   return (
     <div className='min-h-screen bg-base-100'>
         <Navbar />
